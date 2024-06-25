3bac3e4ca0a19351363aa946af17b3839f706165
Ricardo Garza <rgarza0416@yahoo.com>
2024-06-25 00:25:20 -0500

    weight line chart basic ui

--- Step Tracker/Charts/StepPieChart.swift
+++ Step Tracker/Charts/StepPieChart.swift
@@ -78,5 +78,5 @@ struct StepPieChart: View {
 }
 
 #Preview {
-    StepPieChart(chartData: ChartMath.averageWeekdayCount(for: HealthMetric.mockData))
+    StepPieChart(chartData: ChartMath.averageWeekdayCount(for: MockData.steps))
 }
---
+++ Step Tracker/Charts/WeightLineChart.swift
@@ -0,0 +1,55 @@
+//
+//  WeightLineChart.swift
+//  Step Tracker
+//
+//  Created by Ricardo Garza on 6/24/24.
+//
+
+import SwiftUI
+import Charts
+
+struct WeightLineChart: View {
+    
+    var selectedStat: HealthMetricContent
+    var chartData: [HealthMetric]
+    
+    var body: some View {
+        VStack {
+            NavigationLink(value: selectedStat) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Label("Weight", systemImage: "figure")
+                            .font(.title3.bold())
+                            .foregroundStyle(.indigo)
+                        
+                        Text("Avg: 180 lbs")
+                            .font(.caption)
+                    }
+                    Spacer()
+                    
+                    Image(systemName: "chevron.right")
+                }
+            }
+            .foregroundStyle(.secondary)
+            .padding(.bottom, 12)
+            
+            Chart {
+                ForEach(chartData) { weight in
+                    
+                    AreaMark(x: .value("Day", weight.date, unit: .day), y: .value("Value", weight.value))
+                        .foregroundStyle(Gradient(colors: [.blue.opacity(0.5), .clear]))
+                    
+                    LineMark(x: .value("Day", weight.date, unit: .day), y: .value("Value", weight.value))
+                }
+            }
+            .frame(height: 150)
+            
+        }
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+    }
+}
+
+#Preview {
+    WeightLineChart(selectedStat: .weight, chartData: MockData.weights)
+}

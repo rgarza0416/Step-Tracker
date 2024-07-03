00bbc376a97a27b91bd6952b0613d9f71276f631
Ricardo Garza <rgarza0416@yahoo.com>
2024-07-03 14:13:16 -0500

    implement haptic feedback on charts

--- Step Tracker/Charts/WeightLineChart.swift
+++ Step Tracker/Charts/WeightLineChart.swift
@@ -11,6 +11,7 @@ import Charts
 struct WeightLineChart: View {
     
     @State private var rawSelectedDate: Date?
+    @State private var selectedDay: Date?
     
     var selectedStat: HealthMetricContent
     var chartData: [HealthMetric]
@@ -91,6 +92,12 @@ struct WeightLineChart: View {
         }
         .padding()
         .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+        .sensoryFeedback(.selection, trigger: selectedDay)
+        .onChange(of: rawSelectedDate) { oldValue, newValue in
+            if oldValue?.weekdayInt != newValue?.weekdayInt {
+                selectedDay = newValue
+            }
+        }
     }
     
     var annotationView: some View {
